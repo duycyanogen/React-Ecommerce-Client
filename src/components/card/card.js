a74b240b40cd9cca393dc66148a127b1b7608895
duycyanogen <88506379+duycyanogen@users.noreply.github.com>
2022-12-13 22:23:19 +0700

    .

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,38 +2,38 @@
 import './card.scss'
 const { Component } = require("react");
 // import { formatPrice } from '../../constant/temp';
-
+const { Link } = require('react-router-dom');
 export default class Card extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-    }
+  }
 
 
-    render() {
-        return (
-            <a href={'/products/' + this.props.id} className="col-md-3 card-item" onClick={this.props.onclick}>
-                <div className="product-detail">
-                    <div>
-                      <div className="card_img">
-                        <img src={this.props.imageURL} alt="" />
-                      </div>
-                    </div>
-                    <div className='flower-info'>
-                      <p className='product-name'>{this.props.name}</p>
-                      <div className="card_content">
-                        <div className='cart-content__detail'>
-                          <p>Giảm giá {this.props.discount}%</p>
-                          <p>{this.props.views} luợt xem</p>
-                        </div>
-                       {/* <span className='cart-content_price'>{this.props.price}đ</span> */}
-                       <span className='cart-content_price'>{Intl.NumberFormat('it-IT', { style : 'currency', currency:'VND'}).format(this.props.price)}</span>
+  render() {
+    return (
+      <Link to={`/products/${this.props.id}`} className="col-md-3 card-item" onClick={this.props.onclick}>
+        <div className="product-detail">
+          <div>
+            <div className="card_img">
+              <img src={this.props.imageURL} alt="" />
+            </div>
+          </div>
+          <div className='flower-info'>
+            <p className='product-name'>{this.props.name}</p>
+            <div className="card_content">
+              <div className='cart-content__detail'>
+                <p>Giảm giá {this.props.discount}%</p>
+                <p>{this.props.views} luợt xem</p>
+              </div>
+              {/* <span className='cart-content_price'>{this.props.price}đ</span> */}
+              <span className='cart-content_price'>{Intl.NumberFormat('it-IT', { style: 'currency', currency: 'VND' }).format(this.props.price)}</span>
 
-                    </div>
-                    </div>
-                    
-                </div>
-            </a>
-        )
-    }
+            </div>
+          </div>
+
+        </div>
+      </Link>
+    )
+  }
 }
